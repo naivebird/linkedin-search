626a8cf61a18e260c63905755068332d1d1c7bd9
Duc Ho <duc@tenpoint7.com>
2020-03-30 19:48:11 +0700

    remove cached property

--- linkedin_search/decorators.py
+++ linkedin_search/decorators.py
@@ -55,20 +55,3 @@ def login_required(func):
         return func(*args, **kwargs)
 
     return wrapper
-
-
-class cached_property(object):
-    def __init__(self, func, name=None, doc=None):
-        self.__name__ = name or func.__name__
-        self.__module__ = func.__module__
-        self.__doc__ = doc or func.__doc__
-        self.func = func
-
-    def __get__(self, obj, type=None):
-        if obj is None:
-            return self
-        value = obj.__dict__.get(self.__name__, None)
-        if value is None:
-            value = self.func(obj)
-            obj.__dict__[self.__name__] = value
-        return value
